7ba1411c8c60d4f73b85f92f1e90b2bd7d51ad9a
Matz Hilven <48355802+MatzHilven@users.noreply.github.com>
2024-06-19 22:09:48 +0200

    feat: basic command system (#14)

--- src/main/java/com/slampvp/factory/FactoryServer.java
+++ src/main/java/com/slampvp/factory/FactoryServer.java
@@ -1,5 +1,6 @@
 package com.slampvp.factory;
 
+import com.slampvp.factory.command.FactoryCommand;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
@@ -10,15 +11,16 @@ import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.instance.block.BlockHandler;
-import net.minestom.server.utils.NamespaceID;
-import org.jetbrains.annotations.NotNull;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+import java.util.stream.Stream;
+
 
 public final class FactoryServer {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(FactoryServer.class);
 
     public static void main(String[] args) {
@@ -40,20 +42,27 @@ public final class FactoryServer {
             player.setRespawnPoint(new Pos(0, 40, 0));
         });
 
-        Block.GRASS_BLOCK.withHandler(new BlockHandler() {
-
-            @Override
-            public void onDestroy(@NotNull BlockHandler.Destroy destroy) {
-                LOGGER.debug("break at {" + destroy.getBlockPosition() + "}");
-            }
-
-            @Override
-            public @NotNull NamespaceID getNamespaceId() {
-                return NamespaceID.from("minecraft:grass_block");
-            }
-        });
+        streamPackage("com.slampvp.factory.command", FactoryCommand.class).forEach(c ->
+                MinecraftServer.getCommandManager().register(c)
+        );
 
         minecraftServer.start("0.0.0.0", 25565);
+    }
+
+    public static <T> Stream<T> streamPackage(String packageName, Class<T> clazz) {
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<? extends T>> subTypes = reflections.getSubTypesOf(clazz);
+
+        return subTypes.stream()
+                .map(subClass -> {
+                    try {
+                        return clazz.cast(subClass.getDeclaredConstructor().newInstance());
+                    } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
+                             InvocationTargetException e) {
+                        return null;
+                    }
+                })
+                .filter(java.util.Objects::nonNull);
     }
 
 }
